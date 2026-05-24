09bbca5dfec51eaabdba308919125ba5496ba3b4
rajmaddheshiya-art <https://github.com/onlyplaypluse-bit/onlyPlus.git>
2026-05-24 20:33:16 +0530

    fix: production cookie settings with sameSite none

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -39,8 +39,8 @@ export const signup = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             maxAge: 7 * 24 * 60 * 60 * 1000,
-            sameSite: "lax",
-            secure: false
+            sameSite: "none",
+            secure: true
         })
 
         return res.status(201).json(createdUser)
@@ -72,8 +72,8 @@ export const login = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             maxAge: 7 * 24 * 60 * 60 * 1000,
-            sameSite: "lax",
-            secure: false
+            sameSite: "none",
+            secure: true
         })
         return res.status(200).json(existUserName)
     } catch (error) {
