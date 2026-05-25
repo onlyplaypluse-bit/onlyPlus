c6699bdea1c800026c16749206207af372bdd951
rajmaddheshiya-art <https://github.com/onlyplaypluse-bit/onlyPlus.git>
2026-05-25 11:35:42 +0530

    Mera badhiya sa commit message

--- client/src/pages/ContextAPI.jsx
+++ client/src/pages/ContextAPI.jsx
@@ -1,8 +1,8 @@
 import React, { createContext } from "react"
 export const dataContext = createContext()
 function ContextAPI({ children }) {
-    const serverUrl = "http://localhost:8000"
-    // const serverUrl = "https://onlyplus-backend.onrender.com"
+    // const serverUrl = "http://localhost:8000"
+    const serverUrl = "https://onlyplus-backend.onrender.com"
 
 
     const value = {
