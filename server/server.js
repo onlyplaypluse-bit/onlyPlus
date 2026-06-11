007586472309c6449281d3688f621e2d725a51e9
rajmaddheshiya-art <https://github.com/onlyplaypluse-bit/onlyPlus.git>
2026-06-11 16:55:51 +0530

    Updated CORS origin with new domain onlyplus.fun

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const port = process.env.PORT || 5000;
 const app = express()
 
 app.use(cors({
-    origin: ["http://localhost:5173", "http://192.168.43.163:5173", "https://onlyplus-zbgw.onrender.com"],
+    origin: ["http://localhost:5173", "http://192.168.43.163:5173", "https://onlyplus.fun","https://www.onlyplus.fun"],
     methods: ["GET", "POST", "DELETE"],
     credentials: true
 }))
@@ -29,7 +29,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5173", "http://192.168.43.163:5173", "https://onlyplus-zbgw.onrender.com"],
+        origin: ["http://localhost:5173", "http://192.168.43.163:5173", "https://onlyplus.fun","https://www.onlyplus.fun"],
         methods: ["GET", "POST"],
         credentials: true
     }
